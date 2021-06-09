69e88643ed0f4a091482e4a7585b1b6f4c3af7db
Semenykhin Maksym <>
2021-06-09 14:54:23 +0300

    Update index.php

--- app/frontend/views/site/index.php
+++ app/frontend/views/site/index.php
@@ -88,7 +88,7 @@ $this->title = 'My Yii Application';
                         }
                     }
 
-                    echo '<div class="line" ><div class="'.$color.'" style="height:'.$value.'px" ></div><span>'.$row['statements'].'</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
+                    echo '<div class="line" ><div class="'.$color.'" style="height:'.$value.'px" ></div><span>'.$row['statements'].'%</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
                 }
                 ?>
             </div>
@@ -111,7 +111,7 @@ $this->title = 'My Yii Application';
                         }
                     }
 
-                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['branches'].'</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
+                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['branches'].'%</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
                 }
                 ?>
             </div>
@@ -133,7 +133,7 @@ $this->title = 'My Yii Application';
                         }
                     }
 
-                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['lines'].'</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
+                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['lines'].'%</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
                 }
                 ?>
             </div>
@@ -157,7 +157,7 @@ $this->title = 'My Yii Application';
                         }
                     }
 
-                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['functions'].'</span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
+                    echo '<div class="line" ><div class="'.$color.'"  style="height:'.$value.'px" ></div><span>'.$row['functions'].'%/span><span class="rotate">'.unixToData($row['created_at']).'</span></div>';
                 }
                 ?>
             </div>
